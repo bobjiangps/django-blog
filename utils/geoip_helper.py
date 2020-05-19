1a1416c3358aade30e4d1d08faf23f45dedcd641
bobjiangps <jbsv43@sina.com>
2020-05-19 15:40:12 +0800

    update geoip util to get data from service

--- utils/geoip_helper.py
+++ utils/geoip_helper.py
@@ -41,3 +41,19 @@ class GeoIpHelper:
             except URLError:
                 location = "network error"
         return location
+
+    @classmethod
+    def get_location_by_remote_service(cls, ip=None):
+        if not ip:
+            ip = cls.get_ip()
+        if ip.find("error") >= 0 or ip.find(".") < 0:
+            location = "ip data error"
+        else:
+            try:
+                api_key = 'at_IW99hSbVb4uxQq1SbaoIanDbulTbU'
+                api_url = 'https://geo.ipify.org/api/v1?'
+                url = api_url + 'apiKey=' + api_key + '&ipAddress=' + ip
+                location = loads(urlopen(url).read().decode('utf8'))["location"]
+            except URLError:
+                location = "network error"
+        return location
