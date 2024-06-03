c113199c95bc33a6420383e08871746be84333b7
bobjiangps <jbsv43@sina.com>
2024-06-03 11:11:07 +0800

    add basic api accounting

---
+++ api_accounting/views.py
@@ -0,0 +1,14 @@
+from .models import Account
+from .serializers import AccountSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    # 如果想禁用某一个方法，可以类似如下，但不如用generics.RetrieveUpdateAPIView
+    def destroy(self, request, *args, **kwargs):
+        return Response({"message": "delete action is not allowed to 'Account'", "code": "200"}, status=status.HTTP_200_OK)
