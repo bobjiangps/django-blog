5def24716cd33f1b8d81feface170c59b540b42e
bobjiangps <jbsv43@sina.com>
2024-06-19 11:06:13 +0800

    update default account api

--- accounting/views_api.py
+++ accounting/views_api.py
@@ -10,9 +10,10 @@ class AccountViewSet(viewsets.ModelViewSet):
     serializer_class = AccountSerializer
 
     def get_queryset(self):
-        queryset = Account.objects.filter(user_id=self.request.user.id).order_by("-id")
+        # limit the returned data only belongs to current user, cannot view/update another user's account
+        queryset = Account.objects.filter(user_id=self.request.user.id).order_by("id")
         return queryset
 
     # 如果想禁用某一个方法，可以类似如下，但不如用generics.RetrieveUpdateAPIView
     def destroy(self, request, *args, **kwargs):
-        return Response({"message": "delete action is not allowed to 'Account'", "code": "200"}, status=status.HTTP_200_OK)
+        return Response({"message": "delete action is not allowed to 'Account'", "code": "400"}, status=status.HTTP_400_BAD_REQUEST)
